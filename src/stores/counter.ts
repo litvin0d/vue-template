579a641838f604355b1d999e37cd3163ff345bff
Daniil Litvinov <daniil.litvinov@xpage.ru>
2024-02-07 20:11:54 +0500

    initial

---
+++ src/stores/counter.ts
@@ -0,0 +1,10 @@
+/*
+ * Import { computed, ref } from 'vue';
+ * import { defineStore } from 'pinia';
+ *
+ * defineStore('counter', () => {
+ * 	const count = ref(0),
+ * 	 doubleCount = computed(() => count.value * 2);
+ * 	return { count, doubleCount };
+ * });
+ */
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+	</div>
+</template>
+
+<style lang="sass">
+.about
+	@media (min-width: 1024px)
+		min-height: 100vh
+		display: flex
+		align-items: center
+</style>
